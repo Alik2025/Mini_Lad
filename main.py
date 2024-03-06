485750861017a0a9710f4acc7b1f0aa4f8cfc17a
Alik2025 <aikiraaoki-daquip@student.midpac.edu>
2024-03-06 13:41:42 -1000

    3/5 Unpushed changes

---
+++ main.py
@@ -0,0 +1,15 @@
+from pywebio.input import
+from pywebio.output import
+from pywebio.session import
+
+
+
+
+def get_fun_fact(_):
+    clear()
+
+    put_html("<p align= "" left""><h2><img src=" "https://
+    media.geeksforgeeks.org/wp-content/uploads/
+    20210720224119/MessagingHappyicon.png" " Width=" "7%" ">
+    Fun Fact Generator</h2></p>
+")
